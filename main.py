4e5d1e0c99c02350bb031c771ad5fed25603aa40
Marhadiasha Kusumawardhana <marhadiasak@icloud.com>
2020-07-06 10:36:10 +1000

    threshold wip

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
+import sys
+import cv2 as cv
+
+from directoryreader import DirectoryReader
+from thresholder import Thresholder
+
 if __name__ == '__main__':
-    print('Hello, world!')
+    # Check argv
+    if len(sys.argv) < 2:
+        sys.exit(
+            'Please provide the path to images sequence directory.\n'\
+            f'Example: python3 {sys.argv[0]} path/to/images'
+        )
+
+    # Get filepaths
+    directory_reader = DirectoryReader(sys.argv[1])
+    filepaths = directory_reader.get_filepaths()
+
+    # Test
+    image = cv.imread(filepaths[0], cv.IMREAD_GRAYSCALE)
+    thresholder = Thresholder(image)
+    thresholded_image = thresholder.threshold()
+    cv.imwrite(thresholded_image)
+
---
+++ thresholder.py
@@ -0,0 +1,6 @@
+class Thresholder:
+    def __init__(self, image):
+        self.image = image
+
+    def threshold(self):
+        pass
