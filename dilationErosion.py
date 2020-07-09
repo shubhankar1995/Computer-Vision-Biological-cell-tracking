a8db18bfd6c7987d35197a273997581fb39f7bb5
Shubhankar Mathur <shubhankar1995@gmail.com>
2020-07-10 00:44:23 +1000

    chnages for dilate and erode

--- dilationErosion.py
+++ dilationErosion.py
@@ -1,14 +1,30 @@
 import cv2 as cv
+import numpy as np
 
 class DilationErosion:
     def __init__(self, image):
         self.image = image
 
-    def performDilation(self):
-        pass
+    def performDilation(self, img):
+        kernel = np.ones((30,30), np.uint8) 
+        img_dilation = cv.dilate(img, kernel, iterations=1) 
+        return img_dilation
 
-    def performErosion(self):
-        pass
+    def performErosion(self, img):
+        kernel = np.ones((30,30), np.uint8) 
+        img_erosion = cv.erode(img, kernel, iterations=1) 
+        return img_erosion
 
     def getDilateErode(self):
-        pass
+        img_dilation = self.performDilation(self.image)
+        img_erosion = self.performErosion(img_dilation)
+        return img_erosion
+
+    def getErodeDilate(self):
+        img_erosion = self.performErosion(self.image)
+        img_dilation = self.performDilation(img_erosion)
+        return img_dilation
+    
+    def getBorder(self):
+        img_dilation = self.performDilation(self.image)
+        return img_dilation.astype('int') - self.image.astype('int')
--- preprocessor.py
+++ preprocessor.py
@@ -3,6 +3,7 @@ import cv2 as cv
 from contrast_stretcher import ContrastStretcher
 from min_max_filter import MinMaxFilter
 from thresholder import Thresholder
+from dilationErosion import DilationErosion
 
 class Preprocessor:
     def __init__(self, image):
@@ -10,17 +11,28 @@ class Preprocessor:
 
     def preprocess(self):
         image = self.image
-        cv.imwrite('results/original.png', image)          # TODO: remove
+        cv.imwrite('results/1_original.png', image)          # TODO: remove
         image = ContrastStretcher(image).stretch()
-        cv.imwrite('results/stretched.png', image)                # TODO: remove
+        cv.imwrite('results/2_stretched.png', image)                # TODO: remove
         image = cv.medianBlur(image, 5)
-        cv.imwrite('results/blurred.png', image)              # TODO: remove
+        cv.imwrite('results/3_blurred.png', image)              # TODO: remove
+        image = cv.medianBlur(image, 5)
+        cv.imwrite('results/3_2_blurred.png', image)              # TODO: remove
         image = cv.fastNlMeansDenoising(image)
-        cv.imwrite('results/denoised.png', image)                # TODO: remove
-        image = MinMaxFilter(image).filter()
-        cv.imwrite('results/filtered.png', image)               # TODO: remove
-        image = ContrastStretcher(image).stretch() 
-        cv.imwrite('results/post-stretched.png', image)     # TODO: remove
+        cv.imwrite('results/4_denoised.png', image)                # TODO: remove
+        
+        image4 = DilationErosion(image).getBorder()
+        cv.imwrite('results/9_border.png', image4) 
+        
+        
+        # image = MinMaxFilter(image).filter()        
+        # cv.imwrite('results/5_filtered.png', image)               # TODO: remove
+        image = ContrastStretcher(image4).stretch() 
+        cv.imwrite('results/9_2_post-stretched.png', image)     # TODO: remove
         image = Thresholder(image).threshold() 
-        cv.imwrite('results/thresholded.png', image)            # TODO: remove
+        cv.imwrite('results/9_3_thresholded.png', image)            # TODO: remove
+        # image2 = DilationErosion(image).getDilateErode() 
+        # cv.imwrite('results/8_DilateErode.png', image2)            # TODO: remove
+        # image3 = DilationErosion(image).getErodeDilate() 
+        # cv.imwrite('results/9_ErodeDilate.png', image3)            # TODO: remove
         return image
