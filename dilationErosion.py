fe6aef45ffdf0a5da58a198a2881f66f87b365aa
Shubhankar Mathur <shubhankar1995@gmail.com>
2020-07-10 00:42:50 +1000

    added dilation and Erosion

---
+++ dilationErosion.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+
+class DilationErosion:
+    def __init__(self, image):
+        self.image = image
+
+    def performDilation(self):
+        pass
+
+    def performErosion(self):
+        pass
+
+    def getDilateErode(self):
+        pass
